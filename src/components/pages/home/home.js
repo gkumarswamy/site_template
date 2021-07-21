e94654aed9aabbabccece6ff4446dbd7ce7b1aec
Kumar <kumaraswamyg@sssbi.com>
2021-07-21 16:28:12 +0530

    sec2 completed

--- src/components/pages/home/home.js
+++ src/components/pages/home/home.js
@@ -4,7 +4,93 @@ import './home.css';
 const Home = () => {
     return (
         <div>
-          Home
+          <section class="position-relative">
+            <img className="w-100" src="./images/bg-home.jpg" alt="" />
+            <div className="full-width-position-box">
+              <div className="d-flex h-100 align-items-end">
+                <div className="col-12 col-md-5 m-4 p-5 text-white bg-color1">
+                  <h3>Welcome to the Global Partner Success Team</h3>
+                  <p className="fs-20 m-0">Raising the Performance, Quality and Compliance bar to derive greater value from our suppliers, delight our customers and enhance the Service Centre brand.</p>
+                </div>
+              </div>
+            </div>
+          </section>
+          <section class="position-relative bg-color2 py-3">
+            <div className="d-flex align-items-stretch">
+              <div className="col-12 col-md-9 px-3">
+                <div className="row mb-3 px-1">
+                  <div className="col-12 position-relative col-md-4 px-2 ">
+                    <img className="w-100" src="./images/ChildCooking_Empower_CES_CCE-1.jpg" alt="" />
+                    <div className="full-width-position-box">
+                      <div className="d-flex h-100 align-items-end">
+                        <div className="col-12 ps-5 pb-3  text-white   ">
+                          <h3 className="fs-22">Explore GPS</h3>
+                        </div>
+                      </div>
+                    </div> 
+                  </div><div className="col-12 position-relative col-md-4 px-2 ">
+                    <img className="w-100" src="./images/ChildCooking_Empower_CES_CCE-1.jpg" alt="" />
+                    <div className="full-width-position-box">
+                      <div className="d-flex h-100 align-items-end">
+                        <div className="col-12 ps-5 pb-3  text-white   ">
+                          <h3 className="fs-22">Explore GPS</h3>
+                        </div>
+                      </div>
+                    </div> 
+                  </div><div className="col-12 position-relative col-md-4 px-2 ">
+                    <img className="w-100" src="./images/ChildCooking_Empower_CES_CCE-1.jpg" alt="" />
+                    <div className="full-width-position-box">
+                      <div className="d-flex h-100 align-items-end">
+                        <div className="col-12 ps-5 pb-3  text-white   ">
+                          <h3 className="fs-22">Explore GPS</h3>
+                        </div>
+                      </div>
+                    </div> 
+                  </div>
+                </div>
+                <div className="row mb-3 px-1">
+                  <div className="col-12 position-relative col-md-4 px-2 ">
+                    <img className="w-100" src="./images/ChildCooking_Empower_CES_CCE-1.jpg" alt="" />
+                    <div className="full-width-position-box">
+                      <div className="d-flex h-100 align-items-end">
+                        <div className="col-12 ps-5 pb-3  text-white   ">
+                          <h3 className="fs-22">Explore GPS</h3>
+                        </div>
+                      </div>
+                    </div> 
+                  </div><div className="col-12 position-relative col-md-4 px-2 ">
+                    <img className="w-100" src="./images/ChildCooking_Empower_CES_CCE-1.jpg" alt="" />
+                    <div className="full-width-position-box">
+                      <div className="d-flex h-100 align-items-end">
+                        <div className="col-12 ps-5 pb-3  text-white   ">
+                          <h3 className="fs-22">Explore GPS</h3>
+                        </div>
+                      </div>
+                    </div> 
+                  </div><div className="col-12 position-relative col-md-4 px-2 ">
+                    <img className="w-100" src="./images/ChildCooking_Empower_CES_CCE-1.jpg" alt="" />
+                    <div className="full-width-position-box">
+                      <div className="d-flex h-100 align-items-end">
+                        <div className="col-12 ps-5 pb-3  text-white   ">
+                          <h3 className="fs-22">Explore GPS</h3>
+                        </div>
+                      </div>
+                    </div> 
+                  </div>
+                </div>
+               
+              </div>
+              <div className="col-12 col-md-3 p-3 bg-white text-center">
+                <img className="col-10" src="./images/gps-logo.png" alt="" />
+                <h5 className="my-4">The GPS Team</h5>
+                <div className="text-left">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
+                </div>
+                <button className="btn bg-color1 text-white">Meet The Team</button>
+              </div>
+            </div>
+           
+          </section>
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 import './assets/css/fontsize.min.css'
 import './index.css';
 import App from './App';
